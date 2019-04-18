d0750591ab5f5cef1531fd040a81c6a06993f99e
Praval Kumar <Praval.Kumar@hcl.com>
2019-04-18 16:03:26 +0530

    Changed Files-2

--- src/main/java/com/infotech/app/controller/HelloWorldController.java
+++ src/main/java/com/infotech/app/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
 	@RequestMapping(value="/")
 	public String hello() {
-		return "HCL PCF Jenkins Demo - using GitHub test";
+		return "HCL PCF Jenkins Demo - using GitHub test-4:00 pm";
 	}
 }
